fd939da4c7599fdc953acc8b476693a4cc6ad885
Tuan Le <tuanle2@carp.vn>
2022-08-02 01:49:41 +0700

    update Order service

---
+++ MyAspNetCore-Microservices/Order.Domain/Common/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.Domain.Common;
+
+public class EntityBase
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+    public string LastModifiedBy { get; set; }
+}
