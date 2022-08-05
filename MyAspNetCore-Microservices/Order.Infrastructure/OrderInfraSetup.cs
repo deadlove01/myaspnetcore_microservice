09f8e3890a25f6bfe3300e9ea2a6d3bfcf936fb7
tuan le <tuanle2@carp.vn>
2022-08-05 13:40:07 +0700

    update migrations

--- MyAspNetCore-Microservices/Order.Infrastructure/OrderInfraSetup.cs
+++ MyAspNetCore-Microservices/Order.Infrastructure/OrderInfraSetup.cs
@@ -12,7 +12,7 @@ public static class OrderInfraSetup
     public static IServiceCollection AddOrderInfraService(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<OrderContext>(opt =>
-            opt.UseSqlServer(configuration.GetConnectionString("OrderConnectionString")));
+            opt.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
 
         services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
         services.AddScoped<IOrderRepository, OrderRepository>();
--- MyAspNetCore-Microservices/readme.md
+++ MyAspNetCore-Microservices/readme.md
@@ -1,1 +1,6 @@
 docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+
+
+
+dotnet ef migrations add initdb -s Order.API/Order.API.csproj -p Order.Infrastructure/Order.Infrastructure.csproj -c OrderContext
+
