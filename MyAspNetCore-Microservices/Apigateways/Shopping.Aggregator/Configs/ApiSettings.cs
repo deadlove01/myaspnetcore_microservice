596f387813b80153279ad4818d612db3d757558d
Tuan Le <tuanle2@carp.vn>
2022-08-10 01:29:55 +0700

    update shopping aggregator

---
+++ MyAspNetCore-Microservices/Apigateways/Shopping.Aggregator/Configs/ApiSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Configs;
+
+public class ApiSettings
+{
+    public string CatalogUrl { get; set; }
+
+    public string BasketUrl { get; set; }
+
+    public string OrderUrl { get; set; }
+}
---
+++ MyAspNetCore-Microservices/Apigateways/Shopping.Aggregator/Configs/AppServiceSetup.cs
@@ -0,0 +1,29 @@
+﻿using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Configs;
+
+public static class AppServiceSetup
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var apiSettings = new ApiSettings();
+        configuration.Bind("ApiSettings", apiSettings);
+        
+        services.AddHttpClient<ICatalogService, CatalogService>(c =>
+        {
+            c.BaseAddress = new Uri(apiSettings.CatalogUrl);
+        });
+        
+        services.AddHttpClient<IOrderService, OrderService>(c =>
+        {
+            c.BaseAddress = new Uri(apiSettings.OrderUrl);
+        });
+        
+        services.AddHttpClient<IBasketService, BasketService>(c =>
+        {
+            c.BaseAddress = new Uri(apiSettings.BasketUrl);
+        });
+
+        return services;
+    }
+}
