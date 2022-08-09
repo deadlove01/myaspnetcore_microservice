90e5e66b66ab09dd2e34edadeead894a2af99da7
Tuan LM <tuanle2@carp.vn>
2022-08-09 14:08:04 +0700

    update ocelot api gateway

--- MyAspNetCore-Microservices/Catalog.API/Controllers/CatalogController.cs
+++ MyAspNetCore-Microservices/Catalog.API/Controllers/CatalogController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Catalog.API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/v1/[controller]")]
 public class ProductController : ControllerBase
 {
     private readonly IProductRepo _repository;
